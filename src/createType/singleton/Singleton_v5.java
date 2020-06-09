0824f1d570c67bf71d84aa251df1376612248f66
xianchao.hua <hxc_97@163.com>
2020-06-09 10:37:57 +0800

    DCL单例方式加入代码注释分析

--- src/createType/singleton/Singleton_v5.java
+++ src/createType/singleton/Singleton_v5.java
@@ -5,7 +5,7 @@ package createType.singleton;
  * ===但该方法涉及到volitile，需要深入学习
  * 1：私有化构造方法
  * 2：private static volatile修饰instance --volatile实现禁止JVM指令重排序，
- *      不加volatile也会有多线程安全的问题，但是不太好模拟
+ *      不加volatile也会有多线程安全的问题，但是不太好模拟，即DCL失效问题
  * 优缺点暂时不做分析
  * @author: xianchao.hua
  * @create: 2020-06-08 13:16
@@ -18,11 +18,21 @@ public class Singleton_v5 {
     }
 
     public static Singleton_v5 getInstance() throws InterruptedException {
+        //第一次判断为了避免不必要的加锁
         if (instance == null) {
+            //配合测试方法模拟多线程同时进入此处
             Thread.sleep(10);
+            //加锁
             synchronized (Singleton_v5.class) {
-                Thread.sleep(10);
+                //第二次判断+instance定义时用volatile修饰，避免DCL失效问题
                 if (instance == null) {
+                    //new一个对象在JVM中包含以下三个步骤
+                    //  1.在堆内存开辟内存空间。
+                    //  2.在堆内存中实例化SingleTon里面的各个参数。
+                    //  3.把对象指向堆内存空间。
+                    //  由于jvm存在乱序执行功能，所以可能在2还没执行时就先执行了3，
+                    // 如果此时再被切换到线程B上，由于执行了3，INSTANCE 已经非空了，
+                    // 会被直接拿出来用，这样的话，就会出现异常。这个就是著名的DCL失效问题。
                     instance = new Singleton_v5();
                 }
             }
